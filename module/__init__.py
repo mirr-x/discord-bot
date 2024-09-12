d2a53699525f00bb34d30385e78633d8e72ad8b0
mirr <121735620+mirr-x@users.noreply.github.com>
2024-09-12 03:02:01 +0100

    Create __init__.py

---
+++ module/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# __init__.py
+
+import time
+import json
+from selenium.webdriver.common.by import By
+
+# Local module imports
+from .discordPrinte import PrinteDiscord
+from .keep_space_open import KeepSpaceOpen
