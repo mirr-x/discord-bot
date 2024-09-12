4d273ab0ab54194fa72e4b481fe936b725134dad
mirr <121735620+mirr-x@users.noreply.github.com>
2024-09-12 03:04:40 +0100

    Create discordPrinte.py

---
+++ module/discordPrinte.py
@@ -0,0 +1,14 @@
+import requests
+import os 
+
+
+class PrinteDiscord:
+    def __init__(self, data):
+        self.data = {"content": data}  # Ensure data is in the correct format
+        self.url = os.environ['DISCORD_WBHOCK']
+        self.send()
+
+    def send(self):
+        response = requests.post(self.url, json=self.data)
+        if response.status_code != 204:
+            print(f'Failed to send message to Discord: {response.status_code}, {response.text}')
