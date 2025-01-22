74b156c3c724eb434627448016936ccd82bd1605
mirr <121735620+mirr-x@users.noreply.github.com>
2025-01-22 15:18:26 +0100

    Create discordPrinte.py

---
+++ new-cmd/discordPrinte.py
@@ -0,0 +1,14 @@
+import requests
+import os 
+
+
+class PrinteDiscord:
+    def __init__(self, data):
+        self.data = {"content": data}  # Ensure data is in the correct format
+        self.url = 'https://discord.com/api/webhooks/1330874159772860507/zt759H8wUIhbk_YsrgjDc3FSBm-ZDDi3z0Pj3g3etRPXt99nJEZoyxdVafi_OzYfEHdZ'
+        self.send()
+
+    def send(self):
+        response = requests.post(self.url, json=self.data)
+        if response.status_code != 204:
+            print(f'Failed to send message to Discord: {response.status_code}, {response.text}')
