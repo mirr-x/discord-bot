8fc12670262f8b7101a257b131c0b556dae80f3d
mirr <121735620+mirr-x@users.noreply.github.com>
2025-01-22 15:20:49 +0100

    Update discordPrinte.py

--- new-cmd/discordPrinte.py
+++ new-cmd/discordPrinte.py
@@ -2,7 +2,7 @@ import requests
 import os 
 
 
-class PrinteDiscord:
+class PrinteDiscordcopy:
     def __init__(self, data):
         self.data = {"content": data}  # Ensure data is in the correct format
         self.url = 'https://discord.com/api/webhooks/1330874159772860507/zt759H8wUIhbk_YsrgjDc3FSBm-ZDDi3z0Pj3g3etRPXt99nJEZoyxdVafi_OzYfEHdZ'
